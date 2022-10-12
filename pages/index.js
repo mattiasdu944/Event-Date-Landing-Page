dac19a833e958911833fca5204ece9ff79cceb67
Mattias <duartemattias3@gmail.com>
2022-10-11 23:08:09 -0400

    se agrego contenido de la seccion principal

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-import { Text } from '@chakra-ui/react'
-import React from 'react'
 import Layout from './components/Layout'
+
+import styles from '../styles/Main.module.css'
+import {Container, Text } from '@chakra-ui/react'
+
 
 const Home = () => {
   return (
     <Layout>
-      Hola Mundo
+      <section className={`${styles.section} ${styles.home_section}`}>
+        <Container maxW='container.lg' py='5rem'>
+          <Text fontSize={{ base: '14vw', sm:'6xl', md:'7xl', lg:'8xl' }} textAlign={{base:'center', md:'start'}} fontWeight={800} lineHeight={1.2}>
+            Encuentra los <br/><span> Eventos </span> <br/> mas importantes.
+          </Text>
+          <Text fontSize={{base:'md', sm:'lg'}}  textAlign={{base:'center', md:'start'}} mt={5}>
+            La plataforma diseñada para que puedas publicar <br/> o encontrar los eventos que quieras asistir
+          </Text>
+          <a className={styles.button} href='##'>
+            Inicar Sesion
+          </a>
+        </Container>
+      </section>
     </Layout>
   )
 }
